d8eba6f9f4c1d5b3798b4b21b4da330ee436419e
Izabela Perenc <izarenc@onet.eu>
2021-11-22 09:14:01 +0100

    Removed protected uploads

--- step.sh
+++ step.sh
@@ -4,16 +4,6 @@ set -ex
 cd ${gradlew_path}
 
 
-if (./gradlew tasks | grep -q "Protected" ) && ${protected};
-then
-	if ${debug}
-	then
-	    ./gradlew uploadToAppSweepDebugProtected
-	else
-	    ./gradlew uploadToAppSweepReleaseProtected
-	fi
-fi
-
 if ${debug}
 then
     ./gradlew uploadToAppSweepDebug
