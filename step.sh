ebef36cfc1ed5125e56e069f87f1ef48f2dfbfe6
Izabela Perenc <izarenc@onet.eu>
2021-12-17 08:33:02 +0100

    step uses given paths to gradlew

--- step.sh
+++ step.sh
@@ -1,33 +1,49 @@
 #!/bin/bash
 set -ex
-cd ${gradlew_path}
-BUILD_FILE=./app/build.gradle
+
+cd ${project_location}
+
 if [ ! -f "$BUILD_FILE" ]; then
-    echo "$BUILD_FILE does not exist. Probably you are using not standard project sturcture, please add following plugin \"com.guardsquare.appsweep\" to build.gradle script."
-    exit 1
-fi
-if grep -Fq "\"com.guardsquare.appsweep\"" $BUILD_FILE
-then
-    echo "AppSweep plugin exists in $BUILD_FILE"
+    echo "$BUILD_FILE does not exist. Probably you are using not standard project structure, please add following plugin \"com.guardsquare.appsweep\" to build.gradle script."
 else
-    echo "AppSweep plugin was not found in $BUILD_FILE. Injecting..."
-    #remove all white lines
-    sed -i '/^[[:space:]]*$/d' $BUILD_FILE
-    #remove new line character after 'plugins' hopefully before '{' if not we inject new one
-    sed -i '/plugins/{N;s/\n//;}' $BUILD_FILE
-    #inject AppSweep plugin
-    sed -i 's/plugins\s*{/plugins\n{\n\tid "com.guardsquare.appsweep" version "0.1.6"\n/1' $BUILD_FILE
-    if grep -Fq "\"com.guardsquare.appsweep\"" $BUILD_FILE
-    then
-        echo "AppSweep plugin succesfully injected to $BUILD_FILE"
-    else
-        echo "AppSweep plugin was not injected to $BUILD_FILE. Please add following plugin \"com.guardsquare.appsweep\" to build.gradle script manually."
-        exit 1
-    fi   
+	if grep -Fq "\"com.guardsquare.appsweep\"" $BUILD_FILE
+	then
+		echo "AppSweep plugin exists in $BUILD_FILE"
+	else
+		echo "AppSweep plugin was not found in $BUILD_FILE. Injecting..."
+		#remove all white lines
+		sed -i '/^[[:space:]]*$/d' $BUILD_FILE
+		#remove new line character after 'plugins' hopefully before '{' if not we inject new one
+		sed -i '/plugins/{N;s/\n//;}' $BUILD_FILE
+		#inject AppSweep plugin
+		sed -i 's/plugins\s*{/plugins\n{\n\tid "com.guardsquare.appsweep" version "0.1.6"\n/1' $BUILD_FILE
+		if grep -Fq "\"com.guardsquare.appsweep\"" $BUILD_FILE
+		then
+			echo "AppSweep plugin succesfully injected to $BUILD_FILE"
+		else
+			echo "AppSweep plugin was not injected to $BUILD_FILE. Please add following plugin \"com.guardsquare.appsweep\" to build.gradle script manually."
+		exit 1
+		fi   
+	fi
 fi
+
+if [ -f "${gradlew_path}" ];
+then
+	echo "The gradlew file was found in ${gradlew_path}"
+	GRADLEW={$gradlew_path}
+elif [ -f "./gradlew" ];
+then
+	echo "The gradlew file was found in ${project_location}/gradlew"
+	GRADLEW=./gradlew
+else
+	echo "The gradlew file was not found please provide correct gradlew_path"
+	exit 1
+fi
+	
 if ${debug}
 then
-    ./gradlew uploadToAppSweepDebug
+    "$GRADLEW" uploadToAppSweepDebug
 else
-    ./gradlew uploadToAppSweepRelease
+    "$GRADLEW" uploadToAppSweepRelease
 fi
+
