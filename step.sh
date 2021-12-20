a0ec36f2b6d33b6dba16b83db50edebd9dc5f31b
Izabela Perenc <izarenc@onet.eu>
2021-12-20 12:58:23 +0100

    use input api key

--- step.sh
+++ step.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 set -ex
 
-if [[ ! -z "${APPSWEEP_API_KEY}" ]]; then
-  export APPSWEEP_API_KEY=${appsweep_api_key}
-fi
+export APPSWEEP_API_KEY=${appsweep_api_key}
+
 
 cd ${project_location}
 BUILD_FILE=./app/build.gradle
